d613a4001c8703bea011fe08016dfdc507366cad
HMZ-rajput <hamza15nov2000@gmail.com>
2024-07-26 15:45:44 +0500

    skilll page structure done

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,65 @@
+export const skillsData=[
+    {
+        label: "FRONT END",
+        data:[
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "70",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "90",
+            },
+            {
+                skillName: "BOOTSTRAP",
+                percentage: "60",
+            },
+            {
+                skillName: "REACT",
+                percentage: "60",
+            },
+        ]
+    },
+    {
+        label: "BACK END",
+        data:[
+            {
+                skillName: "C#",
+                percentage: "95",
+            },
+            {
+                skillName: "ASP.NET",
+                percentage: "90",
+            },
+            {
+                skillName: "JAVA",
+                percentage: "80",
+            },
+            {
+                skillName: "PHP",
+                percentage: "80",
+            },
+        ]
+    },
+    {
+        label: "DATABASE",
+        data:[
+            {
+                skillName: "SQLSERVER",
+                percentage: "90",
+            },
+            {
+                skillName: "MONGODB",
+                percentage: "50",
+            },
+            {
+                skillName: "SQLITE",
+                percentage: "70",
+            },
+        ]
+    },
+]
